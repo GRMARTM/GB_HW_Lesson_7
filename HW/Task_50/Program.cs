afc0e35c9cdcb47dc2980248fa60e737b19d9292
GRMARTM <120740684+GRMARTM@users.noreply.github.com>
2023-02-09 23:24:31 +0300

    Домашние задание к 7 семинару

---
+++ HW/Task_50/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+1 7 -> такого числа в массиве нет
+1 1 -> 9 */
+
+int[,] arr = new int[5, 5];
+
+FillArray(arr);
+void FillArray(int[,] arra)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-9, 1000);
+        }
+    }
+}
+Console.WriteLine("Введите два числа:");
+string[] sArr = Console.ReadLine().Split(' ');
+int rows = Convert.ToInt32(sArr[0]);
+int colums = Convert.ToInt32(sArr[1]);
+
+Console.WriteLine("В заданном массиве:");
+
+PrintArr(arr);
+
+void PrintArr(int[,] arry)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+if (rows < arr.GetLength(0) && colums < arr.GetLength(1)) 
+{
+    Console.WriteLine(arr[rows, colums]);
+}
+else Console.WriteLine($"{rows} {colums} -> такого числа в массиве нет");
---
+++ HW/Task_52/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
+
+int[,] arr = new int[5, 5];
+
+FillArray(arr);
+void FillArray(int[,] arra)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+Console.WriteLine("В заданном массиве:");
+PrintArr(arr);
+void PrintArr(int[,] arry)
+
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Среднее арифметическое каждого столбца: ");
+
+double[] arithmeticMean = new double[arr.GetLength(1)];
+
+for (int i = 0; i < arr.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < arr.GetLength(0); j++)
+    {
+        result += arr[j, i];
+    }
+    arithmeticMean[i] = result / arr.GetLength(0);
+}
+PrintArray(arithmeticMean);
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i],4}");
+    }
+    Console.WriteLine();
+}
